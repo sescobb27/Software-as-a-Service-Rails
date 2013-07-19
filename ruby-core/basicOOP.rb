f75a307e472e66a5dc9f6f2e00404e8c74ef9320
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-07-19 11:48:57 -0500

    inheritance and polymorphism

---
+++ ruby-core/basicOOP.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby -wKU
+
+# HW 1-4: Basic Object Oriented Programming 
+# OOP herencia y polimorfismo
+class Dessert
+  attr_accessor :name, :calories
+    def initialize(name, calories)
+        @name = name
+        @calories = calories
+    end
+
+    def healthy?
+        @calories < 200
+    end
+
+    def delicious?
+        true
+    end
+end
+
+class JellyBean < Dessert
+    attr_accessor :flavor
+    def initialize(name, calories, flavor)
+        super name, calories
+        @flavor = flavor
+    end
+
+    def delicious?
+        if @flavor == "black licorice" then false
+        else super
+        end
+    end
+end
