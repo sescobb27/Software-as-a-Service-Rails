b6caf1a564862972d6da883b22dddf977ac92c7c
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-08-13 00:01:48 -0500

    moved to it's own repo

--- rails/rottenpotatoes/app/models/movie.rb
+++
@@ -1,9 +0,0 @@
-class Movie < ActiveRecord::Base
-
-  def self.ratings
-    @valid_ratings ||= {'G' => true,'PG' => true,'PG-13' => true,'R' => true}
-  end
-  def self.reset_rating
-    @valid_ratings = nil
-  end
-end
